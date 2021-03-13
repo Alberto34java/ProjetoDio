3ef384508fd773d72b6a3e04afbfea882b8b0de2
Alberto Lacerda <alberto.p.lacerda@hotmail.com>
2021-03-13 14:35:48 -0300

    Criado ProdutoController

---
+++ ProdutosApi/Controllers/ProdutoController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using ProdutosApi.Models;
+using ProdutosApi.Services;
+
+namespace ProdutosApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProdutoController : ControllerBase
+    {
+        private readonly ProdutoServices _services;
+
+        public ProdutoController(ProdutoServices services)
+        {
+            _services = services;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Produto>> Listar()=>_services.ListarProdutos();
+
+        [HttpGet("{id:length(24)}")]
+        public ActionResult<Produto> ExibirProduto(string id)=> _services.ExibirProduto(id);
+        [HttpPost]
+        public ActionResult<Produto> Salvar(Produto produto)=> _services.Salvar(produto);
+
+        [HttpPut]
+        public ActionResult<Produto> Atualizar(string id,Produto produto)=>
+        _services.AtualizarProduto(id,produto);
+
+        [HttpDelete]
+        public ActionResult<Produto> Delete(string id)=> _services.RemoverProduto(id);
+    }
+}
